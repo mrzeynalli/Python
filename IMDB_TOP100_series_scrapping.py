8fa293a10ac502a99349defa64ea333ef8842ab0
MrZzz <88821568+mrzeynalli@users.noreply.github.com>
2022-02-26 18:44:19 +0400

    Add files via upload

---
+++ IMDB_TOP100_series_scrapping.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+url = 'https://www.imdb.com/search/title/?count=100&languages=en&num_votes=1000,&sort=num_votes,desc&title_type=tv_series'
+response = requests.get(url)
+
+soup = BeautifulSoup(response.content, 'lxml')
+
+series_data = soup.findAll('div', attrs= {'class': 'lister-item mode-advanced'})
+
+print(series_data)
+
+name = []
+imdb = []
+years = []
+length = []
+genre = []
+votes = []
+
+for series in series_data:
+    series_name = series.h3.a.text
+    name.append(series_name)
+
+    imdb_score = series.find('div', attrs= {'class': 'inline-block ratings-imdb-rating'}).strong.text
+    imdb.append(imdb_score)
+
+    years_spent = series.find('span', attrs= {'class': 'lister-item-year text-muted unbold'}).text
+    years.append(years_spent)
+
+    serie_lenght = series.find('span', attrs={'class': 'runtime'})
+    serie_lenght = serie_lenght.get_text(strip=True) if serie_lenght else "-"
+    length.append(serie_lenght)
+
+    serie_genre = series.find('span', attrs= {'class': 'genre'}).text
+    genre.append(serie_genre)
+
+    serie_votes = series.find('span', attrs={'name': 'nv'}).text
+    votes.append(serie_votes)
+
+
+series_DF = pd.DataFrame({'Name': name, 'IMDB': imdb, 'Years': years, 'Lenght': length, 'Genre': genre, 'Votes': votes})
+
+series_DF.to_excel("TOP_100_Series.xlsx")
---
+++ Turbo_az_scrapping.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+url = 'https://turbo.az/autos?page=5&q%5Bbarter%5D=0&q%5Bcurrency%5D=azn&q%5Bloan%5D=0&q%5Bmake%5D%5B%5D=41&q%5Bmodel%5D%5B%5D=970&q%5Bprice_from%5D=&q%5Bprice_to%5D=&q%5Bregion%5D%5B%5D=&q%5Bsort%5D=&q%5Byear_from%5D=&q%5Byear_to%5D='
+response = requests.get(url)
+soup1 = BeautifulSoup(response.content, 'html.parser')
+cars = soup1.find_all('div', attrs={'class':'products-i'})
+
+price = []
+model = []
+distance = []
+engine = []
+year = []
+date_and_city = []
+
+for car in cars:
+    car_price = car.find('div', attrs={'class':'product-price'}).get_text()
+    price.append(car_price)
+
+    car_model = car.find('div', attrs={'class': 'products-i__name products-i__bottom-text'}).get_text()
+    model.append(car_model)
+
+    car_distance_covered = car.find('div', attrs={'class': 'products-i__attributes products-i__bottom-text'}).get_text()[14:]
+    distance.append(car_distance_covered)
+
+    car_engine = car.find('div', attrs={'class': 'products-i__attributes products-i__bottom-text'}).get_text()[6:11]
+    engine.append(car_engine)
+
+    car_year = car.find('div', attrs={'class': 'products-i__attributes products-i__bottom-text'}).get_text()[:4]
+    year.append(car_year)
+
+    car_city_datetime = car.find('div', attrs={'class': 'products-i__datetime'}).get_text()
+    date_and_city.append(car_city_datetime)
+
+cars_dataframe = pd.DataFrame({'Price' : price, 'Model' : model, 'Distance' : distance, 'Engine' : engine, 'Year' : year, 'Date and City' : date_and_city})
+
+cars_dataframe.to_excel('Malibu_data5.xlsx')
+
+
+
+
+
+
